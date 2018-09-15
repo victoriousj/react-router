4e79966a3ee80736bb22545b523890667129818e
VictoriousJ <victordjohnson@gmail.com>
2018-09-15 17:30:50 -0500

    refactored the courses componnents so that we only need to use oneinstead of three

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import 
+{ 
+  BrowserRouter, 
+  Route, 
+  Switch 
+} from 'react-router-dom';
 
 import Home from  './Home';
 import About from './About';
 import Header from './Header';
 import Courses from './Courses';
 import Teachers from './Teachers';
+import NotFound from  './NotFound';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
-      <Route path={"/"} component={Header} />
-      <Route exact path={"/"} component={Home} />
-      <Route path={"/about"} component={About} />
-      <Route path={"/courses"} component={Courses} />
-      <Route path={"/teachers"} component={Teachers} />
+    <Header />
+      <Switch>
+        <Route path={"/"} exact component={Home} />
+        <Route path={"/about"} component={About} />
+        <Route path={"/courses"} component={Courses} />
+        <Route path={"/teachers"} component={Teachers} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Route, NavLink, Redirect } from 'react-router-dom';
 
-import CSS from './courses/CSS';
-import HTML from './courses/HTML';
-import JavaScript from './courses/JavaScript';
+import CourseContainer from './courses/CourseContainer';
+import { CSSCourses, HTMLCourses, JSCourses } from '../data/courses';
 
 const Courses = ({match}) => (
   <div className="main-content courses">
@@ -17,9 +16,9 @@ const Courses = ({match}) => (
     </div>
 
     <Route exact path={`${match.path}`} render={ () => <Redirect to={`${match.path}/html`} />} />
-    <Route path={`${match.path}/html`} component={HTML} />
-    <Route path={`${match.path}/css`} component={CSS} />
-    <Route path={`${match.path}/javascript`} component={JavaScript} />
+    <Route path={`${match.path}/html`} render={() => <CourseContainer data={HTMLCourses}/>} />
+    <Route path={`${match.path}/css`} render={() => <CourseContainer data={CSSCourses}/>} />
+    <Route path={`${match.path}/javascript`} render={() => <CourseContainer data={JSCourses}/>} />
   </div>
 );
 
